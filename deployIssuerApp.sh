88cd4ff16782c20e893779d33ef1ab4844c4ea1e
Andrei <andreibobirica99@gmail.com>
2023-06-12 10:38:35 +0200

    Correzione errori fatti da git

--- deployIssuerApp.sh
+++ deployIssuerApp.sh
@@ -1,14 +1,4 @@
-<<<<<<< HEAD:deployIssuer_app.sh
-<<<<<<< HEAD:deployIssuerapp.sh
-#!/bin/bash
-
 cd ./issuerApp
-=======
-cd ./issuer_app
->>>>>>> main:deployIssuer_app.sh
-=======
-cd ./issuerApp
->>>>>>> main:deployIssuerApp.sh
 npm install 
 node index.js & 
 npm start
