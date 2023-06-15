2d38c97b91be14c8cff8b48a2d8e7a67066ddc77
TheBesk <46002209+TheBesk@users.noreply.github.com>
2023-06-15 12:04:31 +0200

    Update dump_db.sql

--- issuerApp/dump_db.sql
+++ issuerApp/dump_db.sql
@@ -1,321 +1,43 @@
---
--- PostgreSQL database cluster dump
---
+-- Adminer 4.8.1 PostgreSQL 15.3 (Debian 15.3-1.pgdg110+1) dump
 
-SET default_transaction_read_only = off;
+\connect "issuerApp";
 
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
+CREATE SEQUENCE "IssuerRegister_id_seq" INCREMENT  MINVALUE  MAXVALUE  CACHE ;
 
---
--- Drop databases (except postgres and template1)
---
-
-DROP DATABASE "issuerApp";
-
-
-
-
---
--- Drop roles
---
-
-DROP ROLE admin;
-
-
---
--- Roles
---
-
-CREATE ROLE admin;
-ALTER ROLE admin WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION BYPASSRLS PASSWORD 'SCRAM-SHA-256$4096:6aBevYrtP2v9WA/Q7UvF6A==$05wrJ1MBZ6n34bQe7TfqLJ8CvD1gm0Qr+u3ukmIWSF0=:LBJlRPmLF/qevxvGY7VbHZ8xd4RX3zpVU9qolj9MiEs=';
-
---
--- User Configurations
---
-
-
-
-
-
-
-
-
---
--- Databases
---
-
---
--- Database "template1" dump
---
-
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 15.3 (Debian 15.3-1.pgdg110+1)
--- Dumped by pg_dump version 15.3 (Debian 15.3-1.pgdg110+1)
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-UPDATE pg_catalog.pg_database SET datistemplate = false WHERE datname = 'template1';
-DROP DATABASE template1;
---
--- Name: template1; Type: DATABASE; Schema: -; Owner: admin
---
-
-CREATE DATABASE template1 WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'en_US.utf8';
-
-
-ALTER DATABASE template1 OWNER TO admin;
-
-\connect template1
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
--- Name: DATABASE template1; Type: COMMENT; Schema: -; Owner: admin
---
-
-COMMENT ON DATABASE template1 IS 'default template for new databases';
-
-
---
--- Name: template1; Type: DATABASE PROPERTIES; Schema: -; Owner: admin
---
-
-ALTER DATABASE template1 IS_TEMPLATE = true;
-
-
-\connect template1
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
--- Name: DATABASE template1; Type: ACL; Schema: -; Owner: admin
---
-
-REVOKE CONNECT,TEMPORARY ON DATABASE template1 FROM PUBLIC;
-GRANT CONNECT ON DATABASE template1 TO PUBLIC;
-
-
---
--- PostgreSQL database dump complete
---
-
---
--- Database "issuerApp" dump
---
-
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 15.3 (Debian 15.3-1.pgdg110+1)
--- Dumped by pg_dump version 15.3 (Debian 15.3-1.pgdg110+1)
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
--- Name: issuerApp; Type: DATABASE; Schema: -; Owner: admin
---
-
-CREATE DATABASE "issuerApp" WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'en_US.utf8';
-
-
-ALTER DATABASE "issuerApp" OWNER TO admin;
-
-\connect "issuerApp"
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-SET default_tablespace = '';
-
-SET default_table_access_method = heap;
-
---
--- Name: IssuerRegister; Type: TABLE; Schema: public; Owner: admin
---
-
-CREATE TABLE public."IssuerRegister" (
-    id integer NOT NULL,
+CREATE TABLE "public"."IssuerRegister" (
+    "id" integer DEFAULT nextval('"IssuerRegister_id_seq"') NOT NULL,
     "familyName" character varying,
     "firstName" character varying,
-    email character varying,
-    password character varying
-);
+    "email" character varying,
+    "password" character varying,
+    CONSTRAINT "IssuerRegister_pkey" PRIMARY KEY ("id")
+) WITH (oids = false);
 
+INSERT INTO "IssuerRegister" ("id", "familyName", "firstName", "email", "password") VALUES
+(2,	NULL,	NULL,	NULL,	NULL),
+(123,	'Rossi',	'Mario',	'm',	'm');
 
-ALTER TABLE public."IssuerRegister" OWNER TO admin;
+CREATE SEQUENCE credential_request_personalid_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 START 5 CACHE 1;
 
---
--- Name: credential_request; Type: TABLE; Schema: public; Owner: admin
---
+CREATE TABLE "public"."credential_request" (
+    "personalid" integer DEFAULT nextval('credential_request_personalid_seq') NOT NULL,
+    "personalidfk" integer NOT NULL,
+    "dateofbirth" date NOT NULL,
+    "familyname" character varying NOT NULL,
+    "firstname" character varying NOT NULL,
+    "gender" character varying NOT NULL,
+    "nameandfamilynameatbirth" character varying NOT NULL,
+    "placeobirth" character varying NOT NULL,
+    "status" boolean
+) WITH (oids = false);
 
-CREATE TABLE public.credential_request (
-    personalid integer NOT NULL,
-    personalidfk integer NOT NULL,
-    dateofbirth date NOT NULL,
-    familyname character varying NOT NULL,
-    firstname character varying NOT NULL,
-    gender character varying NOT NULL,
-    nameandfamilynameatbirth character varying NOT NULL,
-    placeobirth character varying NOT NULL,
-    status boolean
-);
+INSERT INTO "credential_request" ("personalid", "personalidfk", "dateofbirth", "familyname", "firstname", "gender", "nameandfamilynameatbirth", "placeobirth", "status") VALUES
+(1,	123,	'1990-03-03',	'a',	'b',	'm',	'c',	'd',	't'),
+(2,	123,	'1990-03-03',	'a',	'b',	'm',	'c',	'd',	't'),
+(3,	123,	'1990-03-03',	'a',	'b',	'm',	'c',	'd',	't'),
+(4,	123,	'1990-03-03',	'a',	'b',	'm',	'c',	'd',	't'),
+(5,	123,	'1990-03-03',	'a',	'b',	'm',	'c',	'd',	't');
 
+ALTER TABLE ONLY "public"."credential_request" ADD CONSTRAINT "credential_request_personalidfk_fkey" FOREIGN KEY (personalidfk) REFERENCES "IssuerRegister"(id) NOT DEFERRABLE;
 
-ALTER TABLE public.credential_request OWNER TO admin;
-
---
--- Data for Name: IssuerRegister; Type: TABLE DATA; Schema: public; Owner: admin
---
-
-COPY public."IssuerRegister" (id, "familyName", "firstName", email, password) FROM stdin;
-123	a	b	ab	ab
-\.
-
-
---
--- Data for Name: credential_request; Type: TABLE DATA; Schema: public; Owner: admin
---
-
-COPY public.credential_request (personalid, personalidfk, dateofbirth, familyname, firstname, gender, nameandfamilynameatbirth, placeobirth, status) FROM stdin;
-1	123	1990-01-01	Rossi	Mario	Maschio	Mario Rossi	Roma	t
-2	123	1990-03-03	a	b	m	c	d	t
-\.
-
-
---
--- Name: IssuerRegister IssuerRegister_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
---
-
-ALTER TABLE ONLY public."IssuerRegister"
-    ADD CONSTRAINT "IssuerRegister_pkey" PRIMARY KEY (id);
-
-
---
--- Name: credential_request credential_request_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
---
-
-ALTER TABLE ONLY public.credential_request
-    ADD CONSTRAINT credential_request_pkey PRIMARY KEY (personalid);
-
-
---
--- Name: credential_request credential_request_personalidfk_fkey; Type: FK CONSTRAINT; Schema: public; Owner: admin
---
-
-ALTER TABLE ONLY public.credential_request
-    ADD CONSTRAINT credential_request_personalidfk_fkey FOREIGN KEY (personalidfk) REFERENCES public."IssuerRegister"(id);
-
-
---
--- PostgreSQL database dump complete
---
-
---
--- Database "postgres" dump
---
-
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 15.3 (Debian 15.3-1.pgdg110+1)
--- Dumped by pg_dump version 15.3 (Debian 15.3-1.pgdg110+1)
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-DROP DATABASE postgres;
---
--- Name: postgres; Type: DATABASE; Schema: -; Owner: admin
---
-
-CREATE DATABASE postgres WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'en_US.utf8';
-
-
-ALTER DATABASE postgres OWNER TO admin;
-
-\connect postgres
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
--- Name: DATABASE postgres; Type: COMMENT; Schema: -; Owner: admin
---
-
-COMMENT ON DATABASE postgres IS 'default administrative connection database';
-
-
---
--- PostgreSQL database dump complete
---
-
---
--- PostgreSQL database cluster dump complete
---
-
+-- 2023-06-15 10:04:01.507076+00
