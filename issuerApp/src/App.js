50506c6ba7208f15a10d73dd679e053c923664a6
teo-corbu <teo.corbu2014@gmail.com>
2023-07-11 16:48:37 +0200

    aggiunta parte grafica Login e Navbar

--- issuerApp/src/App.js
+++ issuerApp/src/App.js
@@ -1,3 +1,7 @@
+/*
+CODICE INIZIALE DI MICHELE
+
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { AppBar, Toolbar, Typography, List, ListItem, ListItemText, Container } from '@mui/material';
@@ -22,7 +26,7 @@ function App() {
     return <Login setToken={setToken} />
   }
   */
-
+/*
   return (
     <Router>
       <AppBar position="static">
@@ -65,3 +69,64 @@ function App() {
 }
 
 export default App;
+*/
+
+/////////////////////////////////////////////////////////////77
+
+
+import React from 'react';
+import { AuthProvider } from 'react-auth-kit';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+
+
+
+
+
+import Register from './components/Register';
+import Login from './components/Login';
+import RequestCredential from './components/RequestCredential';
+import ViewCredentialRequest from './components/ViewCredentialRequest';
+import Logout from './components/Logout';
+import useToken from './components/useToken';
+import { darkTheme } from './components/Themes';
+
+function App() {
+  const { token, setToken } = useToken();
+
+
+
+  return (
+    <Router>
+      <AuthProvider>
+      <ThemeProvider theme={darkTheme}> 
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                issuerApp
+              </Typography>
+              <Button color="inherit" component={Link} to="/">Home</Button>
+              <Button color="inherit" component={Link} to="/Register">Registrati</Button>
+              <Button color="inherit" component={Link} to="/Login">Login</Button>
+              <Button color="inherit" component={Link} to="/RequestCredential">Richiesta di credenziale</Button>
+              <Button color="inherit" component={Link} to="/ViewCredentialRequest">Visualizzazione richieste credenziale</Button>
+              <Button color="inherit"><Logout /></Button>
+            </Toolbar>
+          </AppBar>
+          <Routes>
+            <Route path="/" />
+            <Route path="/Register" element={<Register />} />
+            <Route path="/Login" element={<Login setToken={setToken} />} />
+            <Route path="/RequestCredential" element={<RequestCredential />} />
+            <Route path="/ViewCredentialRequest" element={<ViewCredentialRequest />} />
+          </Routes>
+      </ThemeProvider>
+      </AuthProvider>
+    </Router>
+  );
+}
+
+
+export default App;
+
