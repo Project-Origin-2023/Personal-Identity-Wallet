28de03dbdcb6d0bbcb5cb04a6ac5f262ead09534
alessioandreetto <72388010+alessioandreetto@users.noreply.github.com>
2023-07-27 17:59:56 +0200

    Verifier update

--- verifierApp/src/components/views/Connect.js
+++ verifierApp/src/components/views/Connect.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
@@ -22,70 +23,100 @@ export function Connect() {
     });
   };
 
+  // Funzione per aprire la schermata Walt-ID in un nuovo pannello
+  const handleOpenWaltId = () => {
+    window.open('https://wallet.walt.id/login?sessionId=bce63d78-eaf0-430b-ba8f-d18493226d64', '_blank');
+  };
+
   return (
-      <div>
-        <CssBaseline />
-        
-        <Container component="main" maxWidth="xs">
-          <Grid 
-                container
-                justifyContent="center"
-                alignItems="center"
-                component="form" 
-                onSubmit={handleSubmit} 
-                noValidate 
-                spacing={4}>
-            <Grid item xs={12}>
-              <Card>
-                <CardContent>
-                  <Typography gutterBottom variant="h3" component="div">
-                    Connect to a Wallet
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Per accedere al servizio devi presentare una credenziale valida collegando un tuo wallet.
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item xs={12}>
-              <Divider variant="contained" />
-            </Grid>
-            <Grid item xs={8}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="walletURI"
-                label="Wallet URI"
-                name="walletURI"
-                autoComplete="Wallet URI"
-                autoFocus
-              />
-            </Grid>
-            <Grid item xs={4}>
-              <Button
-                type="submit"
-              >
-                Connect
-              </Button>
-            </Grid>
-            <Grid item xs={12}>
-              <Divider variant="contained" > <Chip label="OR" /></Divider>
-            </Grid>
-            <Grid item xs={12}>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Connect with Origin Wallet
-              </Button>
-            </Grid>
+    <div>
+      <CssBaseline />
+
+      <Container component="main" maxWidth="xs">
+        <Grid
+          container
+          justifyContent="center"
+          alignItems="center"
+          component="form"
+          onSubmit={handleSubmit}
+          noValidate
+          spacing={4}
+        >
+          <Grid item xs={12}>
+            <Card>
+              <CardContent>
+                <Typography gutterBottom variant="h3" component="div">
+                  Connect to a Wallet
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Per accedere al servizio devi presentare una credenziale valida collegando un tuo wallet.
+                </Typography>
+              </CardContent>
+            </Card>
           </Grid>
-          <LicenseLabel sx={{ mt: 8, mb: 4 }} />
-        </Container>
-      </div>
-      
+          <Grid item xs={12}>
+            <Divider variant="contained" />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="password"
+              label="Password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button type="submit" fullWidth variant="contained" color="primary">
+              Log in
+            </Button>
+          </Grid>
+          <Grid item xs={12}>
+            <Divider variant="contained">
+              <Chip label="OR" />
+            </Divider>
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+                Connect with Origin Wallet
+        
+            </Button>
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              type="button"
+              fullWidth
+              variant="contained"
+              color="primary"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={handleOpenWaltId}
+            >
+              Connect with Walt-ID Wallet
+            </Button>
+          </Grid>
+        </Grid>
+        <LicenseLabel sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </div>
   );
 }
--- verifierApp/src/components/views/Home.js
+++ verifierApp/src/components/views/Home.js
@@ -8,10 +8,15 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-
+import { Link } from 'react-router-dom';
 import { LicenseLabel } from '../comps/LicenseLabel';
 
-const cards = [1, 2, 3, 4, 5, 6];
+// Update the cards array to contain 3 elements instead of 6
+const cards = [
+  { id: 1, title: 'Richiedi ISEE', description: 'Valuta la tua situazione economica per accedere a servizi e agevolazioni.' },
+  { id: 2, title: 'Richiedi CUD', description: 'Ottieni il Certificato Unico di Dipendenza per la dichiarazione dei redditi.' },
+  { id: 3, title: 'Deposita 730', description: 'Effettua la dichiarazione dei redditi e scopri le detrazioni fiscali disponibili.' },
+];
 
 export function Home() {
   return (
@@ -36,7 +41,7 @@ export function Home() {
               Servizi
             </Typography>
             <Typography variant="h5" align="center" color="text.secondary" paragraph>
-              Breve descrizione dei servizi offerti
+              Esplora i nostri servizi dedicati per semplificare la gestione delle tue questioni finanziarie.
             </Typography>
           </Container>
         </Box>
@@ -44,7 +49,7 @@ export function Home() {
           {/* End hero unit */}
           <Grid container spacing={4}>
             {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
+              <Grid item key={card.id} xs={12} sm={6} md={4}>
                 <Card
                   sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
                 >
@@ -58,15 +63,15 @@ export function Home() {
                   />
                   <CardContent sx={{ flexGrow: 1 }}>
                     <Typography gutterBottom variant="h5" component="h2">
-                      Servizio demo
+                      {card.title}
                     </Typography>
                     <Typography>
-                      Questo è  un servizio offerto
+                      {card.description}
                     </Typography>
                   </CardContent>
-                  <CardActions>
-                    <Button size="small">Accedi</Button>
-                  </CardActions>
+                  <Link to="/Connect">
+                    <Button size="small">Connettiti</Button>
+                  </Link>
                 </Card>
               </Grid>
             ))}
