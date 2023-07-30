a1ff2bfbc7ed2866d03741f73316543dd4420772
Andrei <andreibobirica99@gmail.com>
2023-07-31 00:41:53 +0300

    Aggiunta file base waltid

--- scripts/deployAll.sh
+++ scripts/deployAll.sh
@@ -5,11 +5,11 @@ sh ./buildVerifierApp.sh
 sh ./buildPoc_waltid_presentation.sh
 
 #Build IDP Container Docker
-if ! [ -d "waltid-idpkit" ]; then
-    sh ./buildWaltIdIdpImage.sh #Creo immagine IDP image
-fi
+#if ! [ -d "waltid-idpkit" ]; then
+#    sh ./buildWaltIdIdpImage.sh #Creo immagine IDP image
+#fi
 
-docker-compose down
-docker-compose up -d #avvia i container necessari
-sleep 30
+docker compose down
+docker compose up -d #avvia i container necessari
+sleep 60
 sh ./restoreDbIssuerApp.sh
