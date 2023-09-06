4a3e2ee17bfe1bfb14b2accb3c78862ea66b046c
Elton <91457853+Eltonibra@users.noreply.github.com>
2023-09-06 21:42:23 +0200

    Test da finire

--- originIssuerApi/utils/tests/Routing.test.js
+++ originIssuerApi/utils/tests/Routing.test.js
@@ -1,7 +1,105 @@
-const request = require('supertest');
-const { Routing } = require('../../utils/Routing.js');
+//const request = require('supertest');
+//const { Routing } = require('../../utils/Routing.js');
 
-describe('POST /auth/register', () => {
+const request = require('supertest');
+const express = require('express');
+const { DataResponse } = require('../../utils/DataResponse'); // Assicurati di importare DataResponse correttamente
+
+// Importa la classe che contiene l'endpoint Express che vuoi testare
+const YourExpressApp = require('../../utils/Routing.js'); // Sostituisci con il percorso corretto
+
+const app = express();
+
+// Configura l'app Express e l'endpoint come descritto nel tuo codice
+const yourApp = new YourExpressApp(app); // Sostituisci con il percorso corretto
+
+// Test per l'endpoint /auth/login
+describe('POST /auth/login', () => {
+  it('should return 500 and "email Missing" if email is missing', async () => {
+    const response = await request(app)
+      .post('/auth/login')
+      .send({ password: 'password123' });
+
+    expect(response.status).toBe(500);
+    expect(response.body).toEqual(new DataResponse(false, 'email Missing'));
+  });
+
+  it('should return 500 and "password Missing" if password is missing', async () => {
+    const response = await request(app)
+      .post('/auth/login')
+      .send({ email: 'test@example.com' });
+
+    expect(response.status).toBe(500);
+    expect(response.body).toEqual(new DataResponse(false, 'password Missing'));
+  });
+
+  it('should return 500 and "email format not valid" if email format is not valid', async () => {
+    const response = await request(app)
+      .post('/auth/login')
+      .send({ email: 'invalid_email', password: 'password123' });
+
+    expect(response.status).toBe(500);
+    expect(response.body).toEqual(new DataResponse(false, 'email format not valid'));
+  });
+
+  it('should return 500 and "password format not valid" if password format is not valid', async () => {
+    const response = await request(app)
+      .post('/auth/login')
+      .send({ email: 'test@example.com', password: 'invalid_password' });
+
+    expect(response.status).toBe(500);
+    expect(response.body).toEqual(new DataResponse(false, 'password format not valid'));
+  });
+
+  // Aggiungi altri test per verificare il comportamento corretto dell'endpoint in altre situazioni
+
+  it('should return 200 and a valid response if authentication is successful', async () => {
+    // Simula una situazione in cui l'autenticazione è valida
+    // Assicurati che il test venga eseguito con un utente valido e password valida
+
+    const response = await request(app)
+      .post('/auth/login')
+      .send({ email: 'valid_email@example.com', password: 'valid_password' });
+
+    expect(response.status).toBe(200);
+    expect(response.body.success).toBe(true);
+    expect(response.body.data).toBeDefined();
+    // Verifica altri dettagli della risposta se necessario
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*describe('POST /auth/register', () => {
   const routing = new Routing();
   routing.configEndpoint();
 //test mancanza email su registrazione (OK)
@@ -38,7 +136,7 @@ it('should return 500 with "Password Missing" message if password is missing', a
 
 
 //test mancanza password su login ()
-it('should return 500 with "Password Missing" message if password is missing', async () => {
+/*it('should return 500 with "Password Missing" message if password is missing', async () => {
     const response = await request(routing.app)  // Utilizza l'istanza di Routing
       .post('/auth/login')
       .send({ email: 'test@example.com' });
@@ -59,8 +157,9 @@ describe('GET /vcsrequests/marital', () => {
         //sicuramente va mandato qualcosa 
         expect(response.status).toBe(500),
         expect(response.body).toEqual({success: false, description: 'Sys_Admin Authorization, lgo in with an User Account' });
-    });
+    });*/
   
     // ... Altri test simili ...
-  });
+  
+  //});
   
